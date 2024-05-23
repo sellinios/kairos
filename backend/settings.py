486040cc86433b3df3a5dba53578b0b484a9c815
sellinios <lefteris.broker@gmail.com>
2024-05-23 18:51:21 +0000

    first

--- backend/settings.py
+++ backend/settings.py
@@ -8,9 +8,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["kairos.gr", "www.kairos.gr"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -92,6 +92,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'frontend', 'build', 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
